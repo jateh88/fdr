b7800117e19f0a0a12f32be7a7492bec933e3b37
Jonathan Chukinas (home computer) <chukinas@gmail.com>
2019-07-12 08:14:42 -0400

    wip test_excel

--- tests/excel_tests/test_excel.py
+++ tests/excel_tests/test_excel.py
@@ -1,15 +1,23 @@
 import openpyxl
+from pathlib import Path
 
 
 def test_integers_from_excel():
-    wb = openpyxl.load_workbook('test.xlsx')
-    ws = wb['integer']
-    print(ws.max_row)
-    # for row in range(ws.max_row):
-        # row_contents = dict()
-        # key = ws.cell(1, col).value
-        # value = ws.cell(row, col).value
-        # row_contents[key] = value
-        # worksheet_contents.append(row_contents)
 
-test_integers_from_excel()
+    path = Path(__file__).parent / "test.xlsx"
+    wb = openpyxl.load_workbook(filename=path, read_only=True)
+    ws = wb["integer"]
+    start_row = 1
+    end_row = ws.max_row
+    for row in range(start_row, end_row + 1):
+        assert_type = ws.cell(row, 1).value
+        value = ws.cell(row, 2).value
+        try:
+            value = int(value)
+        except (TypeError, ValueError):
+            # Value stays as-is
+            pass
+        if assert_type == "pass":
+            assert isinstance(value, int)
+        elif assert_type == "fail":
+            assert not isinstance(value, int)
