cdb0429693b0cbc640b4e5af51fe783c7d531e30
Eric Kirchgessner <EKirchge@its.jnj.com>
2019-07-18 16:04:25 -0400

    'ID obj not iterable error' fixed in id.py

--- src/fdr/fields/id.py
+++ src/fdr/fields/id.py
@@ -1,13 +1,38 @@
 from .field import Field
 import click
-# TODO added click module for click.echo.
+
 
 class ID(Field):
-
     _header_names = ['id']
 
-    # TODO Note to Jonathan- I'm not sure if it makes sense to define the validation function here or in validate().
-    #  I copied/pasted it from check_form_functions.py and is called below in the body of validate().
+    def validate(self):
+        # Currently, validate() will only check if a value starts with P and will print a descriptive message when
+        # called. the input argument defined is a list of IDs that was previously extracted from the FDR.
+        # Init. an empty list that will contain the spreadsheet rows where discrepancies are found
+
+        click.echo("Validating ID Field!")
+
+        error_indices = []
+        # for indices (location of errors) and ids (value in cell) in self (input argument) enumerate list starting
+        # with 1. This will offset indices so it can be used to directly reference excel sheet (row#)
+        for indices, ids in enumerate(self._columns, 1):
+            # call validation function to check value and compare to boolean
+            if starts_with_p(ids) is False:
+                # if discrepancy found (validation function is false), append the index to a list
+                error_indices.append(indices)
+        # Determine the length of the error_indices list. this is indicative of the number of errors.
+        # call constructor function (str()) to cast string for output
+        error_count = str(len(error_indices))
+        # call str() to cast the indices to string for output
+        error_rows = str(error_indices)
+        output_string = "Discrepancy Description: ID does not start with P. Discrepancy count: " + error_count + \
+                        ". Found in in Rows: " + error_rows
+        click.echo(output_string)
+        # Other validation actions (future state)
+        # - column exists
+        # - cells not empty
+        # - cells in alphabetical order
+
     # Define validation functions (for demonstration, checking that the value starts with P will be the only one)
     # Targeted column: ID
     # What it does: checks that value has a capital P as the first character.
@@ -18,38 +43,3 @@ class ID(Field):
             return True
         else:
             return False
-
-    # TODO Note to Jonathan- I have tested the body of validate() assuming "self" is a list of strings but
-    #  haven't tested by calling it as a method
-    def validate(self):
-
-        # TODO: misspelling of 'indices'
-        # Currently, validate() will only check if a value starts with P and will print a descriptive message when
-        # called. the input argument defined is a list of IDs that was previously extracted from the FDR.
-        # Init. an empty list that will contain the spreadsheet rows where discrepancies are found
-
-        click.echo("Validating ID Field!")
-
-        error_indeces = []
-        # for indeces (location of errors) and ids (value in cell) in self (input argument) enumerate list starting
-        # with 1. This will offset indeces so it can be used to directly reference excel sheet (row#)
-        for indeces, ids in enumerate(self, 1):
-            # call validation function to check value and compare to boolean
-            if starts_with_p(ids) is False:
-                # if discrepancy found (validation function is false), append the index to a list
-                error_indeces.append(indeces)
-        # Determine the length of the error_indeces list. this is indicative of the number of errors.
-        # call constructor function (str()) to cast string for output
-        error_count = str(len(error_indeces))
-        # call str() to cast the indeces to string for output
-        error_rows = str(error_indeces)
-        output_string = "Discrepancy Description: ID does not start with P. Discrepancy count: " + error_count + \
-                        ". Found in in Rows: " + error_rows
-        click.echo(output_string)
-        # Other validation actions (future state)
-        # - column exists
-        # - cells not empty
-        # - cells in alphabetical order
-
-    def starts_with_p(self):
-        pass
