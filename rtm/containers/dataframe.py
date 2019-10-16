105d0092e98d7f3530b80fe48e784b70e281a467
Jonathan Chukinas (work computer) <jchukina@its.jnj.com>
2019-10-16 16:18:55 -0400

    successfully reading dataframe while seeking header row

--- rtm/containers/dataframe.py
+++ rtm/containers/dataframe.py
@@ -3,6 +3,7 @@ the procedure-based requirements excel worksheet."""
 
 # --- Standard Library Imports ------------------------------------------------
 from difflib import SequenceMatcher
+import statistics
 
 # --- Third Party Imports -----------------------------------------------------
 # None
@@ -11,13 +12,35 @@ from difflib import SequenceMatcher
 # None
 
 
+def find_best_match(value: str, strings: list, return_ratio=False):
+    """Return the best match to a given string."""
+    string = str(value)
+    if len(strings) == 0:
+        if return_ratio:
+            return 0
+        else:
+            return None
+    ratios = [
+        SequenceMatcher(None, string, str(orig_string)).ratio()
+        for orig_string in strings
+    ]
+    max_ratio = max(ratios)
+    if return_ratio:
+        return max_ratio
+    else:
+        index = ratios.index(max_ratio)
+        return strings[index]
+
+
 def map_expected_to_actual(expected_strings: set, actual_strings: set):
     """
-    Used for mapping field names to their exact or best matches in pandas dataframe.
+    Maps field names to their exact or best matches in pandas dataframe.
     :param expected_strings: strings for whom we seek the best match
     :param actual_strings: available strings
     :return: dict[expected_string] = actual_string
     """
+
+    # --- Setup ---------------------------------------------------------------
     result = {}
 
     # --- Get exact matches ---------------------------------------------------
@@ -27,32 +50,32 @@ def map_expected_to_actual(expected_strings: set, actual_strings: set):
     remaining_actual = list(actual_strings - expected_strings)
 
     # --- Order expected strings by best match --------------------------------
-    def sort_by_match_ratio(input_string):
-        return find_best_match(
-            string=input_string,
+    def sort_by_match_ratio_then_alphbetical(input_string):
+        ratio = find_best_match(
+            value=input_string,
             strings=list(remaining_expected),
             return_ratio=True,
         )
-    remaining_expected.sort(key=sort_by_match_ratio)
+        return ratio, input_string
+    remaining_expected.sort(key=sort_by_match_ratio_then_alphbetical)
 
     # --- Find best match for each remaining string ---------------------------
     for string in remaining_expected:
         best_actual = find_best_match(string, remaining_actual)
         result[string] = best_actual
-        remaining_actual.remove(best_actual)
+        if best_actual is not None:
+            remaining_actual.remove(best_actual)
 
     # --- Return result -------------------------------------------------------
     return result
 
 
-def find_best_match(string: str, strings: list, return_ratio=False):
-    """Return the best match to a given string."""
-    ratios = [
-        SequenceMatcher(None, string, orig_string).ratio()
-        for orig_string in strings
-    ]
-    max_ratio = max(ratios)
-    if return_ratio:
-        return max_ratio
-    else:
-        return ratios.index(max_ratio)
+def similarity(input_strings, output_strings):
+    match_ratios = [find_best_match(string, output_strings, True) for string in input_strings]
+    return statistics.mean(match_ratios)
+
+
+if __name__ == '__main__':
+    headers = 'hello good bye'.split()
+    actual = ['hello', 'good', 'bye', 'bye']
+    print(similarity(headers, actual))
--- tests/experiments/pandas_experiment/pandas_.py
+++ tests/experiments/pandas_experiment/pandas_.py
@@ -1,28 +1,78 @@
 import pandas as pd
 import pathlib
+from rtm.containers import dataframe
+import xlrd
+from rtm.main.exceptions import RTMValidatorError
 
 
-path = pathlib.Path(__file__).parent.parent/'test_rtm.xlsx'
-sheet_name = 'pandas_experiment'
-header_row = 1
+def get_df(excel_path, excel_sheet_name, header_row=1):
+    try:
+        return pd.read_excel(
+            io=excel_path,
+            sheet_name=excel_sheet_name,
+            header=header_row-1,
+            dtype=object,
+        )
+    except FileNotFoundError:
+        raise RTMValidatorError(f'<{excel_path}> not valid file.')
+    except xlrd.biffh.XLRDError:
+        raise RTMValidatorError(f"<{excel_sheet_name}> sheet not found")
 
-# pdre = pd.read_excel
-df = pd.read_excel(
-    io=path,
-    sheet_name=sheet_name,
-    header=header_row-1,
-    dtype=object,
-)
 
-print(df)
-print()
-print('HEADERS\n', df[['bye']])
-bye = df['bye']
-print('\n', bye)
-for row in bye.tolist():
-    print(row)
-print(list(bye))
-print(list(df['bye.1']))
+def find_header_row(excel_path, excel_sheet_name, headers_list):
+
+    # --- Get dataframe -------------------------------------------------------
+    try:
+        df = pd.read_excel(
+            io=excel_path,
+            sheet_name=excel_sheet_name,
+            header=None,
+            dtype=object,
+        )
+    except FileNotFoundError:
+        raise RTMValidatorError(f'<{excel_path}> not valid file.')
+    except xlrd.biffh.XLRDError:
+        raise RTMValidatorError(f"<{excel_sheet_name}> sheet not found")
+
+    # --- get list of first 20 rows -------------------------------------------
+    # headers_list = list(df.columns)
+    rows = []
+    for row_index, row in enumerate(df.itertuples()):
+        if row_index == 20:
+            break
+        # row_values = [row.header_name for header_name in headers_list]
+        row_values = list(df.iloc[row_index])
+        rows.append(row_values)
+    # print(rows)
+
+    # --- find best matching row ----------------------------------------------
+    similarities = [dataframe.similarity(headers_list, row) for row in rows]
+    # print(similarities)
+    best_match = max(similarities)
+    best_match_row_index = similarities.index(best_match)
+    return best_match_row_index + 1
+
+
+if __name__ == '__main__':
+    path = pathlib.Path(__file__).parent.parent.parent/'test_rtm.xlsx'
+    sheet_name = 'pandas_experiment'
+    headers = 'hello good bye'.split()
+    try:
+        header_row = find_header_row(path, sheet_name, headers)
+        df = get_df(path, sheet_name, header_row)
+        print(df)
+    except RTMValidatorError as e:
+        print(e)
+
+# print(df)
+# print()
+# print('HEADERS\n', df[['bye']])
+# bye = df['bye']
+# print('\n', bye)
+# for row in bye.tolist():
+#     print(row)
+# print(list(bye))
+# print(list(df['bye.1']))
 
 
 
